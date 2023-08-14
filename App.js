9cd4bfafa778e89c3c0eacd23f5ba301373b1877
orkunkn <orkunkaymann@gmail.com>
2023-08-14 16:02:18 +0300

    Alert, Touchable and Styles

--- App.js
+++ App.js
@@ -1,41 +1,84 @@
 import React, { useState } from 'react';
-
 import {
-  Button,
-  StyleSheet, Text, View,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 const App = () => {
-  const [number, setNumber] = useState(0)
-  const [click, setClick] = useState(0)
 
-  const onClickHandler = () => {
-    setNumber(number+5)
-    setClick(click+1)
+  const [name, SetName] = useState('');
+  const [submitted, SetSubmitted] = useState(false);
+  const onPressHandler = () => {
+    if (name.length > 3)
+      SetSubmitted(!submitted);
+    else {
+      Alert.alert('Warning', 'The name must be longer than 3 characters', [
+        { text: 'OK' },
+        { text: 'No' },
+        { text: 'Yes' },
+      ])
+    }
   }
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{number}</Text>
-      <Button title='Add' onPress={onClickHandler}></Button>
-      <Text style={styles.text}>You clicked {click} times</Text>
+      <Text style={styles.text}>
+        Please write your name:
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. John'
+        onChangeText={(value) => SetName(value)}
+      />
+      <TouchableOpacity
+        onPress={onPressHandler}
+        style={styles.button}
+      >
+        <Text style={styles.text}>
+          {submitted ? 'Clear' : 'Submit'}
+        </Text>
+      </TouchableOpacity>
+      {submitted ?
+        <Text style={styles.text}>
+          You are registered as {name}
+        </Text>
+        :
+        null
+      }
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#0000ff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center'
   },
   text: {
-    color: '#ffffff',
+    color: '#000000',
     fontSize: 20,
-    fontStyle: 'italic',
-    margin: 10
-  }
+    margin: 10,
+  },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  button: {
+    width: 150,
+    height: 50,
+    backgroundColor: '#fff03f',
+    alignItems: 'center',
+  },
 });
 
 export default App;
